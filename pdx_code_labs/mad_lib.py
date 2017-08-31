7b96ecc71aeef87f871c2a38174b58a8eb1b0880
Will <wgmackenz@gmail.com>
2017-08-31 15:38:11 -0700

    init commit

---
+++ pdx_code_labs/mad_lib.py
@@ -0,0 +1,7 @@
+noun1 = input("Give me an animal: ")
+noun2 = input("Give me another animal: ")
+verb1 = input("Now give me a past tense verb: ") # I made this past tense so the flow of the final Mad Libs is smoother.
+verb2 = input("Let's go with one more past tense verb: ") # I made this past tense so the flow of the final Mad Libs is smoother.
+
+print ("Your MadLib") # This line explains what the next line of output will be
+print ("The", noun1, "ran around and", verb1, "the", noun2, "which in turn,", verb2, ", resulting in a most peculiar turn of events.")
